5df39ad6a9c177ff353c6a737bad33b9866d1239
Kaushik Patil <kaushik5678@gmail.com>
2023-04-18 00:58:46 -0500

    Initial commit

--- insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO SERVICE_CATEGORY(name,code) VALUES("House Cleaning","HCL");
-INSERT INTO SERVICE_CATEGORY(name,code) VALUES("Plumbing","PLM");
-INSERT INTO SERVICE_CATEGORY(name,code) VALUES("Pest Control","PCTRL");
-INSERT INTO SERVICE_CATEGORY(name,code) VALUES("Painting","PAINT");
-INSERT INTO SERVICE_CATEGORY(name,code) VALUES("Electrical","ELECT");
-
-
-INSERT INTO SERVICE_REQUEST(service_category_code, customer_id,requested_on) VALUES("HCL", 1, "2023-3-4 20:32:5");
